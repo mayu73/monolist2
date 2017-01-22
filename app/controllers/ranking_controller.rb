7b752fc47d44d47707b56bb1d3eb5144b3e0e565
Mayu <okazakimayu@gmail.com>
2017-01-22 03:22:53 +0000

    modify ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,16 +2,16 @@ class RankingController < ApplicationController
     def have
       @items = Item.joins(:haves)
       .select("items.*","count(ownerships.id) as haves_count")
-      .group("ownerships.item_id")
-      .order("count(ownerships.item_id) desc")
+      .group("items.id")
+      .order("haves_count desc")
       .limit(10)
     end
     
     def want
       @items = Item.joins(:wants)
       .select("items.*","count(ownerships.id) as wants_count")
-      .group("ownerships.item_id")
-      .order("count(ownerships.item_id) desc")
+      .group("item_id")
+      .order("wants_count desc")
       .limit(10)
     end
 end
